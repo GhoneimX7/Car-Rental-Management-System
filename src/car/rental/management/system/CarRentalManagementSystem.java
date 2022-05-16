9783124159424d76544425148f282096f8339d97
Taha Ghoneim <Taha.Ghoneim11@gmail.com>
2022-05-16 03:04:28 +0200

    full project

---
+++ src/car/rental/management/system/CarRentalManagementSystem.java
@@ -0,0 +1,16 @@
+package car.rental.management.system;
+
+import view.Splash;
+
+/**
+ *
+ * @author TahaGhoneim
+ */
+
+public class CarRentalManagementSystem {
+
+    public static void main(String[] args) {
+        new Splash();
+    }
+    
+}
